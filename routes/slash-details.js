af09d7ab0b91692c6e101f74f825e4eb2d289993
w3project <adityabatgeri@gmail.com>
2023-08-19 12:18:54 +0530

    Cart items added sort

--- routes/slash-details.js
+++ routes/slash-details.js
@@ -1,5 +1,6 @@
 const exp = require('express');
 const addProduct = require('../models/products');
+const newProfile = require('../models/profiles');
 const route = exp.Router();
 
 route.get('/details/:ID', async (req,res) => {
@@ -7,7 +8,18 @@ route.get('/details/:ID', async (req,res) => {
 
     let prod;
     await addProduct.fetchProduct(key).then(ans => {prod=ans;});
-    const data = {name:prod.Prod_name, img:prod.Prod_img, color:prod.Prod_color, desc:prod.Prod_desc, price:prod.Prod_price, brand:prod.Prod_brand, model:prod.Prod_model, stock:prod.Prod_stock, id:key};
+
+    const curr = newProfile.getCurrentUser();
+    let arr;
+    await newProfile.checkUsername(curr).then(ans => {arr=ans.Cart;});
+
+    let present=false;
+    for (let i=0; i<arr.length; i++)
+    {
+        if (key==arr[i]) {present=true; break;}
+    }
+
+    const data = {name:prod.Prod_name, img:prod.Prod_img, color:prod.Prod_color, desc:prod.Prod_desc, price:prod.Prod_price, brand:prod.Prod_brand, model:prod.Prod_model, stock:prod.Prod_stock, id:key, present:present};
 
     res.render('details', data);
 })
