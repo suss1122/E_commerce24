57191eec63976f0abcbe5a42d371bead8ab92982
w3project <adityabatgeri@gmail.com>
2023-08-24 20:25:09 +0530

    payment option added

--- public/js/order.js
+++ public/js/order.js
@@ -1,4 +1,4 @@
-var sub = document.getElementsByTagName('input')[3];
+var sub = document.getElementsByTagName('input')[4];
 var input = document.getElementsByTagName('input');
 var text = document.getElementsByTagName('textarea')[0];
 
@@ -29,4 +29,54 @@ text.oninput = () => {
         sub.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
         sub.style.pointerEvents = 'none';
     }
+}
+
+var nullDown = document.querySelectorAll(".pay img");
+for (let j=1; j<10; j=j+2)
+{
+    nullDown[j].style.visibility = 'hidden';
+}
+
+var pay = document.getElementsByClassName("pay")[0];
+pay.style.backgroundColor = 'rgba(200, 200, 255, 0.4)';
+
+var mode = document.getElementsByClassName("mode")[0];
+var down = document.getElementsByClassName("down")[0];
+
+pay.onclick = () => {
+    if (mode.style.maxHeight != 'fit-content')
+    {
+        mode.style.maxHeight = 'fit-content';
+        mode.style.overflowY = 'default';
+        down.style.transform = 'rotate(180deg)';
+    }
+    else 
+    {
+        mode.style.maxHeight = '53.67px';
+        mode.style.overflowY = 'hidden';
+        down.style.transform = 'none';
+    }
+}
+
+var allPay = document.getElementsByClassName("pay");
+var val = document.getElementsByClassName("val");
+var payInp = document.getElementsByTagName("input")[3];
+
+for (let i=1; i<allPay.length; i++)
+{
+    allPay[i].addEventListener("click", () => {
+        if (val[i].innerHTML == 'Cash On Delivery') {payInp.value = 'cash_on_delivery'}
+        else if (val[i].innerHTML == 'Netbanking') {payInp.value = 'netbanking'}
+        else if (val[i].innerHTML == 'UPI Payment') {payInp.value = 'upi'}
+        else if (val[i].innerHTML == 'Credit Card') {payInp.value = 'credit_card'}
+        else if (val[i].innerHTML == 'Debit Card') {payInp.value = 'debit_card'}
+
+        let temp = allPay[0].innerHTML;
+        allPay[0].innerHTML = allPay[i].innerHTML;
+        allPay[i].innerHTML = temp;
+
+        mode.style.maxHeight = '53.67px';
+        mode.style.overflowY = 'hidden';
+        down.style.transform = 'none';
+    })
 }
