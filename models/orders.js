47e637d56b6b65a0474bd5455d68d7762c612505
w3project <adityabatgeri@gmail.com>
2023-08-28 23:51:50 +0530

    Order backend fully complete

--- models/orders.js
+++ models/orders.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 const newProfile = require('./profiles');
 
-const struc = new mongoose.Schema({Username:String, Order_date:String, Products:Array, Order_Id:String});
+const struc = new mongoose.Schema({Username:String, Order_date:String, Products:Array, Order_Id:String, Address:String, Phno:String, Email:String, Payment:String});
 const Order = mongoose.model("order", struc);
 
 module.exports = class newOrder
 {
-    constructor(name, date, arr)
+    constructor(name, date, arr, address, phno, email, pay)
     {
         this.name=name;
         this.date=date;
         this.arr=arr;
         this.order_ID = Math.random().toString();
+        this.address=address;
+        this.phno=phno;
+        this.email=email;
+        this.pay=pay;
     }
 
     async upload()
     {
-        const up = new Order({Username:this.name, Order_date:this.date, Products:this.arr, Order_Id:this.order_ID});
+        const up = new Order({Username:this.name, Order_date:this.date, Products:this.arr, Order_Id:this.order_ID, Address:this.address, Phno:this.phno, Email:this.email, Payment:this.pay});
         up.save();
 
         await newProfile.updateOrder(this.name, this.order_ID).then();
@@ -25,7 +29,7 @@ module.exports = class newOrder
     static async getOrder(ID)
     {
         let temp;
-        await Order.findOne({id:ID}).then(ans => {temp=ans});
+        await Order.findOne({Order_Id:ID}).then(ans => {temp=ans});
         return temp;
     }
 
--- routes/slash-order.js
+++ routes/slash-order.js
@@ -3,6 +3,7 @@ const newProfile = require('../models/profiles');
 const route = exp.Router();
 const path = require('path');
 const newOrder = require('../models/orders');
+const addProduct = require('../models/products');
 
 route.get('/order', (req,res) => {
     if(!req.session.USER){res.redirect('/warn')}
@@ -23,10 +24,31 @@ route.post('/placeorder', async (req,res) => {
     await newProfile.checkUsername(curr).then(ans => {temp=ans.Cart});
 
     // await newProfile.order(curr).then();
-    const obj = new newOrder(curr, req.body.date, temp);
+    const obj = new newOrder(curr, req.body.date, temp, req.body.ADDRESS, req.body.PHNO, req.body.EMAIL, req.body.payment);
     obj.upload();
 
     res.redirect('/account');
+    }
+})
+
+route.get('/ordDet/:ID', async (req,res) => {
+    if(!req.session.USER){res.redirect('/warn')}
+    else{
+    
+    const key = req.params.ID;
+
+    let order;
+    await newOrder.getOrder(key).then(ans => {order=ans});
+
+    const arr=[];
+    for (let i=0; i<order.Products.length; i++)
+    {
+        await addProduct.fetchProduct(order.Products[i]).then(ans => {arr.push(ans)});
+    }
+
+    const data = {orderID:key, orderDate:order.Order_date, arr:arr, orderAddress:order.Address, orderPhno:order.Phno, orderEmail:order.Email, orderPay:order.Payment};
+
+    res.render('orderDet', data);
     }
 })
 
